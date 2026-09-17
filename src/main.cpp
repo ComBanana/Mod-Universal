aff1a7f544c954d61903509c2f720869d3f6f4b5
ComBanana <140673792+ComBanana@users.noreply.github.com>
2026-09-17 19:42:17 +0800

    Move noclip hooks into dedicated module

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,9 @@
 #include <Geode/Geode.hpp>
 #include <Geode/loader/SettingV3.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/PlayerObject.hpp>
 
 #include "../include/ModMenu.hpp"
 
 using namespace geode::prelude;
-
-class $modify(ModUniversalPlayLayer, PlayLayer) {
-    void destroyPlayer(PlayerObject* player, GameObject* object) {
-        if (ModMenu::isNoclipEnabled()) {
-            log::debug("Noclip blocked player death");
-            return;
-        }
-
-        PlayLayer::destroyPlayer(player, object);
-    }
-};
-
-class $modify(ModUniversalPlayerObject, PlayerObject) {
-    bool collidedWithObject(
-        float dt,
-        GameObject* object,
-        CCRect rect,
-        bool skipCheck
-    ) {
-        if (ModMenu::isNoclipEnabled())
-            return false;
-
-        return PlayerObject::collidedWithObject(
-            dt,
-            object,
-            rect,
-            skipCheck
-        );
-    }
-};
 
 $on_mod(Loaded) {
 
